f79c80299e5f4569c1107995d4793faa8078c5b9
script-cloud <charusathish47@gmail.com>
2021-07-22 10:49:01 +0530

    Scanner Code

---
+++ src/scanner/Sample.java
@@ -0,0 +1,18 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Sample {
+	public static void main(String[] args) {
+		Scanner a = new Scanner(System.in);
+		System.out.println("Enter the integer");
+		int nextInt1 = a.nextInt();
+		System.out.println("The integer value is ");
+		System.out.println("Enter the byte value");
+		byte nextByte1 = a.nextByte();
+		System.out.println("The byte value is ");
+		
+	}
+	
+
+}
