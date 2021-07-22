fc770bec1a35bf71a500aab82a622926076d8dd2
script-cloud <charusathish47@gmail.com>
2021-07-22 12:00:57 +0530

    hello git hub

--- src/scanner/Sample.java
+++ src/scanner/Sample.java
@@ -8,11 +8,20 @@ public class Sample {
 		System.out.println("Enter the integer");
 		int nextInt1 = a.nextInt();
 		System.out.println("The integer value is ");
+		float nextFloat = a.nextFloat();	
+		System.out.println(nextFloat);
+
+
+		
+			
+			double nextDouble = a.nextDouble();
+			System.out.println(nextDouble);
 		System.out.println("Enter the byte value");
 		byte nextByte1 = a.nextByte();
 		System.out.println("The byte value is ");
 		long nextLong = a.nextLong();
 		System.out.println("enter long");
+		
 		
 	}
 	
